e1201013f677d678a82afd3c1f51b305800148eb
T0modachi <jvergarava@gmail.com>
2023-10-10 15:03:33 -0300

    move system configuration into a flake

--- system/configuration.nix
+++ system/configuration.nix
@@ -10,6 +10,12 @@
       ./hardware-configuration.nix
     ];
 
+  # JVV nix flakes following https://www.youtube.com/watch?v=mJbQ--iBc1U&t=2s
+  nix.package = pkgs.nixFlakes;
+  nix.extraOptions = ''
+   experimental-features = nix-command flakes
+  '';
+
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
