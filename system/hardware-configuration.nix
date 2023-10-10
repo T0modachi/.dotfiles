8e008ee14f2e6f2ef690c28eeed8a552f7c1b30b
T0modachi <jvergarava@gmail.com>
2023-10-10 18:01:56 -0300

    add bluetooth and pipewire

--- system/hardware-configuration.nix
+++ system/hardware-configuration.nix
@@ -65,4 +65,21 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-}
+
+  # enable bluetooth
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  # Enable sound with pipewire
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+
+
+ }
