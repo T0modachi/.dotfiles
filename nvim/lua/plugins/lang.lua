503c253542d531faa080264f9be135387adf0967
T0modachi <jvergarava@gmail.com>
2024-02-27 02:24:22 +0100

    add: codium, ruby, ts, json, yaml, prettier support to nvim

---
+++ nvim/lua/plugins/lang.lua
@@ -0,0 +1,10 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.ruby" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.lsp.none-ls" }
+}
