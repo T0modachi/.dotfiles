f494b772239210bf1c0ef9732aa8ddcabbd35357
T0modachi <jvergarava@gmail.com>
2024-05-13 15:46:09 +0200

    add mob

--- users/jvergara-buk/home.nix
+++ users/jvergara-buk/home.nix
@@ -31,6 +31,7 @@
     cachix
     glamoroustoolkit
     dbeaver
+    mob
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
