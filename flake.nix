9d2fdcd1c39466c8e5456c8b7c085f2400304246
T0modachi <jvergarava@gmail.com>
2024-06-09 18:11:18 +0200

    allow unfree packages

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs {
       inherit system;
-      config = {allowUnfree = true;};
+      config = {allowUnfree = true; allowUnfreePredicate = (_: true);};
     };
     lib = nixpkgs.lib;
   in  {
