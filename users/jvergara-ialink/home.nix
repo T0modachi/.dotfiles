e7a6c60e7a81985474aa9fa8c0b8a6b292071a8f
T0modachi <jvergarava@gmail.com>
2024-12-04 14:47:16 -0300

    add postman for work

--- users/jvergara-ialink/home.nix
+++ users/jvergara-ialink/home.nix
@@ -42,6 +42,7 @@
     mqttx
     code-cursor
     nfs-utils
+    postman
     # for php debuging
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
