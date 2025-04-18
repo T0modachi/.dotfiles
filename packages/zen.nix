315ffcbd217e8ec18982ef2c42d6e26ce2215a20
T0modachi <jvergarava@gmail.com>
2025-04-17 23:21:22 -0400

    update zen-browser

--- packages/zen.nix
+++ packages/zen.nix
@@ -8,7 +8,7 @@
 
   src = fetchurl {
     url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
-    sha256 = "sha256-1NoDIdzeADVLOhtesR0QZefhAdRBSNWg4++MjqdkfIs=";
+    sha256 = "sha256-aRFKjSw3Wove0k091ndEBjbduxEWOMymfmZpWNZqNVk=";
   };
 
   appimageContents = appimageTools.extract {
