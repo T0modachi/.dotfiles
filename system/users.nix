b3ec0efb6813b7f1dd1ef11edf7a429a20c5f711
T0modachi <jvergarava@gmail.com>
2026-07-18 20:42:14 -0400

    fix zsh not activating on niri

--- system/users.nix
+++ system/users.nix
@@ -1,12 +1,16 @@
+{ pkgs, ... }:
 {
   users.users.T0modachi = {
     isNormalUser = true;
     description = "T0modachi";
+    shell = pkgs.zsh;
     extraGroups = [
       "networkmanager"
       "wheel"
     ];
   };
+
+  programs.zsh.enable = true;
 
   security.sudo.enable = true;
   security.sudo.wheelNeedsPassword = true;
